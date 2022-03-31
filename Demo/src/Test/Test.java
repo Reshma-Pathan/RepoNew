f8067d493a96af9a26ef1e83606a3ce71712302a
Reshma-Pathan <Resh5550@gmail.com>
2022-03-31 09:17:47 +0530

    changed by X

---
+++ Demo/src/Test/Test.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=23,b=7;
+		int s=a+b;
+		
+  	  System.out.println("sum="+s);
+
+	}
+
+}
