f65be1bcb210323684619e961fef55485d116e2e
Reshma-Pathan <Resh5550@gmail.com>
2022-03-31 10:42:40 +0530

    changes by devx

--- Demo/src/Test/Test2.java
+++ Demo/src/Test/Test2.java
@@ -4,7 +4,11 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+              String test="Welcome to java";
+              if(test.contains("java"))
+              {
+            	  System.out.println("Found !!!!");
+              }
 	}
 
 }
